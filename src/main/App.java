a52b073ab9c00ea65b5a7e16b8160cfba946f8a2
Yann Renard <yannrenard1025@gmail.com>
2024-12-03 18:14:54 +0100

    docs(trace_exec): add links to repository's

--- src/main/App.java
+++ src/main/App.java
@@ -8,6 +8,7 @@
  */
 
 import extensions.File;
+import extensions.CSV;
 
 class App extends Program {
 
@@ -22,46 +23,15 @@ class App extends Program {
         int pollution = 20;
         int bonheur = 85;
 
-        afficherArtASCII(DESSIN);
-        println("==========");
-        println(NOM);
-        println("==========");
-        println();
-        print("Appuyer sur entrée pour commencer à jouer");
-        readString();
-        clearScreen();
 
         // // Boucle principale du jeu
         // while (budget >= /* valeur */ || pollution >= /* valeur */ || bonheur >= /* valeur */) {
         //     afficherEtatJeu(tour, budget, pollution, bonheur);
         //     tour++;
         // }
-        println("Fin du jeu !");
     }
 
     // Affichage de l'état du jeu
     void afficherEtatJeu(int tour, int budget, int pollution, int bonheur) {
-        println("-------------------------");
-        println("Tour " + tour);
-        println("Budget : " + budget + " €");
-        println("Pollution : " + pollution + " %");
-        println("Bonheur : " + bonheur + " %");
-        println("-------------------------");
-        println("Actions disponibles :");
-        println("1. Construire une usine");
-        println("2. Planter des arbres");
-        println("3. Organiser un festival");
-        println("-------------------------");
-        print("Choisissez une action (1-3) : ");
-    }
-
-    // Affichage du contenu d'un fichier texte d'après son chemin
-    void afficherArtASCII(String chemin) {
-        File unTexte = newFile(chemin);
-
-        // Stockage dans une variable de la ligne suivante dans le fichier
-        while (ready(unTexte)) {
-            println(readLine(unTexte));
-        }
     }
 }
