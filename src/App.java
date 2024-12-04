af6b305e889232a39889cb966bc7b416870a2692
yanismekki <yanis.mekki.etu@univ-lille.fr>
2024-12-04 16:11:38 +0100

    commit
    Co-authored-by: Yann Renard <yannouuuu@users.noreply.github.com>

--- src/App.java
+++ src/App.java
@@ -15,30 +15,72 @@ class App extends Program {
 
     final String NOM = "EcoManager";
     final String DESSIN = "ressources/art_ascii.txt";
-    final int TEMPS_PAUSE = 500;
 
+    void algorithm(){
+        afficherTitreJeu(DESSIN); // Yann
+        int choix=readInt();
+        while(choix!=1 || choix!=2 || choix!=3 || choix!=4){
+            println("La saisie est invalide veuillez recommencez !");
+            choix=readInt();
+        }
+        if(choix==1){
+            println("Quel est le nom de votre ville ?");
+            String nomville=readString();
+            City ville=creerpartie(nomville);
+        }
+        else if(choix==2){
+            println("Systeme de chargement de sauvegarde pas encore disponible");
+            //Charger partie quand dispo ville=""
+        }
+        else if(choix==3){
+            println("Règles");
+        }
+        if(choix==1 || choix==2){
+            while((ville.tour<70) || (ville.bonheur>50) || (ville.pollution<100) || (ville.budget>0)){
+                afficherEtatJeu(ville);
+                
 
-    void algorithm() {
-        // Initialisation des variables de jeu (A EQUILIBRER)
-        int tour = 1;
-        int budget = 45000;
-        int pollution = 20;
-        int bonheur = 85;
-        https://prod.liveshare.vsengsaas.visualstudio.com/join?81210A5EAEBB322520A4EC8B6065BD617C34
+            }
+        }
+        
+        
 
-
-        // // Boucle principale du jeu
+        
+        
+        // Boucle principale du jeu
         // while (budget >= /* valeur */ || pollution >= /* valeur */ || bonheur >= /* valeur */) {
         //     afficherEtatJeu(tour, budget, pollution, bonheur);
         //     tour++;
         // }
     }
 
+    
+
     // Affichage de l'état du jeu
-    void afficherEtatJeu(int tour, int budget, int pollution, int bonheur) {
+    void afficherEtatJeu(City ville) {
+        println(ville.nom)
+        println("---------------------------------")
+        println("Tour : " + ville.tour);
+        println("Budget : " + ville.budget + " €");
+        println("Pollution : " + ville.pollution + "%");
+        println("Bonheur : " + ville.bonheur + "%");
     }
 
-
-
+    void afficherTitreJeu(String dessin){
+        File file = newFile(dessin);
+        while(ready(files)){
+            println(readLine(files));
+        }
+    }  
+    
+    City creerpartie(String nom){
+        City ville= new City();
+        ville.nom=nom;
+        ville.tour=1;
+        ville.budget=45000;
+        ville.pollution=20;
+        ville.bonheur=85;
+        return ville;
+    }
 }
 
