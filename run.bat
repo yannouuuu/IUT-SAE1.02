fc23dc805b671491b5040f7d70a5379e26ff8c91
Yann Renard <yannrenard1025@gmail.com>
2024-12-12 09:29:43 +0100

    fix: update MAINCLASS in run.bat to point to App.java

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ set CLASSPATH=
 
 for /R lib %%f in (*.jar) do set CLASSPATH=!CLASSPATH!;%%f
 set CLASSPATH=%CLASSPATH%;classes
-set MAINCLASS=EcoManager
+set MAINCLASS=App.java
 
 java -cp "%CLASSPATH%" %MAINCLASS%
